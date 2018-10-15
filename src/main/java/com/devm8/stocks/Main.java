9f4762154b27bc9c38a8d0ea6f36a17fbc3cb1dd
horia <muntean.horia.catalin@gmail.com>
2018-10-15 09:03:35 +0300

    Add spring config

--- src/main/java/com/devm8/stocks/Main.java
+++ src/main/java/com/devm8/stocks/Main.java
@@ -1,12 +1,15 @@
-package com.devm8.stocks.main;
+package com.devm8.stocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.devm8.stocks")
-public class Main {
+@EnableJpaRepositories("com.devm8.stocks.repositories")
+public class Main implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
